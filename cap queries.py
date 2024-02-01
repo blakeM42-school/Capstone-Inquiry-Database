cd33a56b8d363a67b585e2be38a9a5e66034baca
blakeM42-school <141346707+blakeM42-school@users.noreply.github.com>
2024-02-01 13:59:52 -0700

    Update cap queries.py

--- cap queries.py
+++ cap queries.py
@@ -1,9 +1,21 @@
 import pandas as pd
 
-file_path = r"C:\Users\blakemangola\Downloads\SUU-Students-2024-01-25-T-06-30-15.csv"
+file_path = r"C:\Users\hdmia\Desktop\capstone\SUU-Students-2024-01-25-T-06-30-15.csv"
 students = pd.read_csv(file_path, encoding='latin1')
+result = students.loc[students['DEPARTMENT'] == "Computer Science/Cybersecurity"]
+result1 = students.loc[students['DEPARTMENT'] == "Aviation Sciences"]
+result2 = students.loc[students['DEPARTMENT'] == "Engineering and Technology"]
 
+result.to_csv(r"C:\Users\hdmia\Desktop\capstone\computerscienceandcybersecurity.csv")
+result1.to_csv(r"C:\Users\hdmia\Desktop\capstone\aviationsciences.csv")
+result2.to_csv(r"C:\Users\hdmia\Desktop\capstone\engineeringandtechnology.csv")
 
+row_count = result.shape[0]
+row_count1 = result1.shape[0]
+row_count2 = result2.shape[0]
+print(row_count)
+print(row_count1)
+print(row_count2)
 #print(students.head(10))
 #x = students.head(10)
 
@@ -18,7 +30,7 @@ students = pd.read_csv(file_path, encoding='latin1')
 #for index, row in students.iterrows():
     #print(index, row['LAST_NAME'])
 
-print(students.loc[students['USERNAME'] == "chamberscameron"])
+
 
 #print(students.describe())
 
